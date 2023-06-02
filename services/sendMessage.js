52f52bc24c638fc363c0404748d72040697cb96f
Somanyu <somanyu.03samal@gmail.com>
2023-06-02 10:45:11 +0530

    add: Service for sending message to the user.

---
+++ services/sendMessage.js
@@ -0,0 +1,26 @@
+
+const sendMessage = async (phone, text) => {
+    console.log("🚀 ~ file: sendMessage.js:3 ~ sendMessage ~ phone:", phone)
+    try {
+        const accountSid = process.env.TWILIO_ACCOUNT_SID;
+        const authToken = process.env.TWILIO_AUTH_TOKEN;
+        const client = require('twilio')(accountSid, authToken);
+
+        const message = client.messages.create({
+            from: 'whatsapp:+14155238886',
+            body: `${text}`,
+            to: `whatsapp:+91${phone}`
+        }).catch(error => {
+            console.log("🚀 ~ file: sendMessage.js:14 ~ sendMessage ~ error:", error)
+            client.messages.create({
+                from: 'whatsapp:+14155238886',
+                body: `${text}`,
+                to: `whatsapp:+91${phone}`
+            })
+        })
+    } catch (error) {
+        console.log("🚀 ~ file: sendMessage.js:22 ~ sendMessage ~ error:", error)
+    }
+}
+
+module.exports = { sendMessage }
