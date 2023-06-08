a84cf0af46ff48ab687663eecb16942b4b6f4932
Somanyu <somanyu.03samal@gmail.com>
2023-06-08 21:13:03 +0530

    update: Update `domain` attribute.

--- controller/authController.js
+++ controller/authController.js
@@ -78,7 +78,7 @@ exports.signIn = async (req, res, next) => {
             expires: new Date(Date.now() + 100 * 30),
             maxAge: 1000 * 60 * 60 * 24 * 7,
             sameSite: 'none',
-            domain: 'app.somanyu.tech',
+            domain: 'finapp.somanyu.tech',
             secure: true,
         });
 
